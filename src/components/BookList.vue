c97f6efdb183a8e38b4d1cbaafdcfd0772e79800
Jack Schuss <jackschuss@gmail.com>
2018-06-27 10:10:10 -0400

    PluralSight - Part 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="book in books">{{ book.title }}: {{book.author}}</li>
+    </ul>
   </div>
-
 </template>
+
 <script>
 export default {
   name: "BookList",
   data() {
     return {
-      title: "All Books"
+      title: "All Books",
+      books: [
+        { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
+        { title: 'American Gods', author: 'Neil Gaiman' },
+        { title: 'Amusing Ourselves to Death', author: 'Neil Postman' },
+      ]
     }
   }
 }
@@ -20,5 +28,15 @@ export default {
 <style>
  h1, h2 {
    font-weight: normal;
+ }
+
+ ul {
+   list-style-type: none;
+   padding: 0;
+ }
+
+ li {
+   display: block;
+   margin: 0 10px;
  }
 </style>
